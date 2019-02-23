7916df94940440fad9feb414ecc0e56340d8a522
Yadhap Dahal <yadhapdahal111@gmail.com>
2019-02-23 11:45:55 -0500

    Updates

---
+++ precache-manifest.76157547933eff504125da85168b5aa6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6126fdabce42ad0692a2",
+    "url": "/toDo/static/js/runtime~main.6126fdab.js"
+  },
+  {
+    "revision": "ea533201897e89741749",
+    "url": "/toDo/static/js/main.ea533201.chunk.js"
+  },
+  {
+    "revision": "8593d4d2833458843ca0",
+    "url": "/toDo/static/js/2.8593d4d2.chunk.js"
+  },
+  {
+    "revision": "ea533201897e89741749",
+    "url": "/toDo/static/css/main.e083ccad.chunk.css"
+  },
+  {
+    "revision": "1d9398f85c57a93cc3689534ff7bb211",
+    "url": "/toDo/index.html"
+  }
+];
